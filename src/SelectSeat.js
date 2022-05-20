e469c7d58f3d435a51d74c97741eea1b5afa15a8
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-05-20 10:36:34 -0300

    Renderização do mapa de assentos e função para mudar layout de assentos selecionados implementada

--- src/SelectSeat.js
+++ src/SelectSeat.js
@@ -1,10 +1,133 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+
+
+function MapOfSeats ({isAvailable, seatName, idSeat, seatsObj, setSeatsObj}) { 
+    
+    function markSeat (idSeat) {
+        const aux = seatsObj;
+        for (let i = 0 ; i < aux.seats.length; i++) {
+            if (idSeat === aux.seats[i].id) {
+                if (aux.seats[i].isAvailable === "Selected") {
+                    aux.seats[i].isAvailable = true;
+                } else {
+                    aux.seats[i].isAvailable = "Selected";
+                }
+            }
+        }
+        setSeatsObj({... aux});
+    }
+    
+
+    if (!isAvailable) {
+        return (
+            <div className="seatButton seatNotAvailable">
+                {seatName}
+            </div>
+        )
+    }
+
+    if (isAvailable === "Selected") {
+        return (
+            <div className="seatButton seatSelected" onClick={() => markSeat(idSeat)}>
+                {seatName}
+            </div>
+        )
+    }
+
+    return (
+        <div className="seatButton Available" onClick={() => markSeat(idSeat)}>
+            {seatName}
+        </div>
+    )
+}
+
+
 
 
 export default function SelectSeat() {
+    
+    const { idSessao } = useParams();
+    
+    const [seatsObj, setSeatsObj] = useState({});
+
+	useEffect(() => {
+		const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSessao}/seats`);
+        
+		promise.then(response => {
+			setSeatsObj(response.data);
+		});
+
+        promise.catch(erro => console.log("Status code:" + erro.response.status))
+        
+	}, []);
+    
+    console.log(seatsObj);
+    
+    
+    
     return (
-        <Link to="/sucesso">
-            <div>SELECT SEAT</div>
-        </Link>
+        <>
+            <div className="pageContainer">
+                <div className="pageTitle">
+                    <span>{`Selecione o(s) assento(s)`}</span>
+                </div>
+
+                <div className="seatsContainer">
+                    <div className="seatsMap">
+                        
+                        {seatsObj.movie ? (
+                            <>
+                                {seatsObj.seats.map((seat, index) => 
+                                    <MapOfSeats 
+                                        key={index} 
+                                        isAvailable={seat.isAvailable}
+                                        seatName={seat.name}
+                                        idSeat={seat.id}
+                                        seatsObj={seatsObj}
+                                        setSeatsObj={setSeatsObj}
+                                    />)}
+                            </>
+                            ) : ("Carregando Assentos...")}
+                    </div>
+
+                    <div className="seatsExamples">
+                        <div className="exampleContainer">
+                            <div className="seatButton seatSelected"></div>
+                            <span>Selecionado</span>
+                        </div>
+                        <div className="exampleContainer">
+                            <div className="seatButton seatAvailable"></div>
+                            <span>Disponível</span>
+                        </div>
+                        <div className="exampleContainer">
+                            <div className="seatButton seatNotAvailable"></div>
+                            <span>Indisponível</span>
+                        </div>
+                        
+                        
+                    </div>
+
+                </div>
+
+            </div>
+
+            
+
+            {seatsObj.movie ? (
+                <div className="footer">
+                    <div className="filmPosterFooter">
+                        <img src={seatsObj.movie.posterURL} alt=""/>
+                    </div>
+                    <div className="infosContainer">
+                        <span>{seatsObj.movie.title}</span>
+                        <span>{`${seatsObj.day.weekday} - ${seatsObj.name}`}</span>
+                    </div>
+                </div>
+            ) : ("Carregando Informações...")}
+
+        </>
     )
 }
